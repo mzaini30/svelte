ea9a457ba89b1df0368bb831f8d4deb4f325ab50
mzaini30 <muhzaini30@gmail.com>
2020-10-28 08:43:57 +0800

    upload

--- script/pakaiPurge.js
+++ script/pakaiPurge.js
@@ -1,9 +1,14 @@
 fs = require('fs')
+const { exec } = require('child_process');
 fs.readdir("public/dist", (_, x) => {
 	x = x.filter(x => x.match(/\.css/g)).filter(x => x != "global.css")
+	// x = ['bootstrap.min.css']
+	for (y of x){
+		exec(`purgecss --css hasil/dist/${x} --content src/**/*.svelte --output hasil/dist2`)
+	}
 	namaFile = "hasil/index.html"
 	fs.readFile(namaFile, "utf8", (_, y) => {
 		y = y.replace(`/dist/${x}`, `/dist2/${x}`)
-		fs.readFile(namaFile, y, "utf8", () => {})
+		fs.writeFile(namaFile, y, "utf8", () => {})
 	})
 })
