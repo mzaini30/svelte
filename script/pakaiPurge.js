131b4fdf76fcd2d59443b7cfd8c8a02ff96e658c
Zen <muhzaini30@gmail.com>
2020-11-01 14:04:02 +0800

    Perbaikan Purge CSS untuk Tailwind

--- script/pakaiPurge.js
+++ script/pakaiPurge.js
@@ -10,7 +10,7 @@ safelist: rule css yang nggak kena purge
 ["html", ".hai"]
 */
 abaikan = []
-safelist = []
+// safelist = []
 
 recursive("public/dist", (_, x) => {
 	x = x.filter(x => x.match(/\.css/g)).filter(x => {
@@ -24,7 +24,10 @@ recursive("public/dist", (_, x) => {
 	new PurgeCSS().purge({
 	  content: ['src/**/*.svelte', 'public/index.html'],
 	  css: [...x],
-	  safelist: [...safelist]
+	  whitelist: ["svg:not(:root).svg-inline--fa"], 
+	  whitelistPatterns: [/^fa-/, /^svg-inline--fa/], 
+	  whitelistPatternsChildren: [/^token/, /^pre/, /^code/], 
+	  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [] 
 	}).then(x => {
 		for (y of x){
 			fs.writeFile(y.file.replace('public/', 'hasil/'), y.css, 'utf8', () => {})
