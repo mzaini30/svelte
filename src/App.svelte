30131a4c737bf16319be8deb5863cf25c19c5218
Zen <muhzaini30@gmail.com>
2020-11-05 10:15:02 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,15 @@
   import { routes } from "../.routify/routes";
   import {isLoading} from '@/store'
   import Loading from '@/tools/Loading.svelte'
+  let vh = 0
+  const tinggiFull = () => {
+  	vh = window.innerHeight * 0.01
+  	document.documentElement.style.setProperty("--vh", `${vh}px`)
+  }
+  tinggiFull()
+  window.addEventListener("resize", tinggiFull)
 </script>
 {#if $isLoading}
 	<Loading/>
 {/if}
-<Router {routes} />
+<Router {routes}/>
