9769470cafde81f067b8d68e420f0600537b2b21
Zen <muhzaini30@gmail.com>
2020-12-04 04:52:25 +0800

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -51,3 +51,6 @@ export function tanggal(){
   const dt = new Date();
   return `${dt.getFullYear().toString().padStart(4,'0')}-${(dt.getMonth()+1).toString().padStart(2,'0')}-${dt.getDate().toString().padStart(2,'0')} ${dt.getHours().toString().padStart(2,'0')}:${dt.getMinutes().toString().padStart(2,'0')}:${dt.getSeconds().toString().padStart(2,'0')}`
 }
+export function sleep(n){
+ return new Promise(resolve => setTimeout(resolve, n))
+}
