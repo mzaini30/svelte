83ea329b0cb9a0f7e3b75a9b11053b42f09f1ba0
mzaini30 <muhzaini30@gmail.com>
2020-12-08 00:16:14 +0800

    oke

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<link rel='stylesheet' href='/dist/bootstrap/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='/dist/global.css'>
+</svelte:head>
+<slot/>
---
+++ upload.bat
@@ -0,0 +1,5 @@
+@echo off
+git status
+git add -A .
+git commit -m "oke"
+git push
