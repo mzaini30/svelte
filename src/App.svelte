ec23a3cf31ce2190bf7b442cc9d0015e90469714
mzaini30 <muhzaini30@gmail.com>
2021-02-20 11:46:37 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,36 @@
 <script>
-  import { Router } from "@roxi/routify";
-  import { routes } from "../.routify/routes";
-  let vh = 0
-  const tinggiFull = () => {
-  	vh = window.innerHeight * 0.01
-  	document.documentElement.style.setProperty("--vh", `${vh}px`)
-  }
-  tinggiFull()
-  window.addEventListener("resize", tinggiFull)
+	import { Router } from "@roxi/routify";
+	import { routes } from "../.routify/routes";
+	let vh = 0
+	const tinggiFull = () => {
+		vh = window.innerHeight * 0.01
+		document.documentElement.style.setProperty("--vh", `${vh}px`)
+	}
+	tinggiFull()
+	window.addEventListener("resize", tinggiFull)
 </script>
+
 <Router {routes}/>
+
+<svelte:head>
+	<link rel='stylesheet' href='/dist/bootstrap.min.css'>
+</svelte:head>
+
+<style type="text/scss">
+	:global {
+		* {
+			word-wrap: break-word;
+		}
+		.h-screen {
+			height: 100vh;
+			height: calc(var(--vh, 1vh) * 100);
+		}
+		.min-h-screen {
+			min-height: 100vh;
+			min-height: calc(var(--vh, 1vh) * 100);
+		}
+		img {
+			max-width: 100%;
+		}
+	}
+</style>
