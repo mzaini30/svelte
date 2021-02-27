041e766135eb6c55ccf4e8b3d78ddfa3861dd7c0
mzaini30 <muhzaini30@gmail.com>
2021-02-27 21:35:59 +0800

    upload

--- public/index.php
+++ public/index.php
@@ -1,20 +1,26 @@
+<?php
+	$judul = 'Svelte Template';
+	$deskripsi = 'Starter tools for building amazing site!';
+	$cover = '/dist/jean.jpg';
+	$icon = '/dist/ungu.png';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset='utf-8'>
 	<meta name='viewport' content='width=device-width,initial-scale=1'>
 
-	<title>Svelte Template</title>
-	<meta name="description" content="Template Svelte yang diolah oleh Zen">
-	<meta property="og:image" content="/dist/jean.jpg">
-	<link rel='icon' type='image/png' href='/dist/ungu.png'>
+	<title><?= $judul ?></title>
+	<meta name="description" content="<?= $deskripsi ?>">
+	<meta property="og:image" content="<?= $cover ?>">
+	<link rel='icon' type='image/png' href='<?= $icon ?>'>
 	
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@blogodop">
 	<meta name="twitter:creator" content="@blogodop">
-	<meta name="twitter:title" content="Svelte Template">
-	<meta name="twitter:description" content="Template Svelte yang diolah oleh Zen">
-	<meta name="twitter:image" content="https://mzaini30.js.org/dist/jean.jpg">
+	<meta name="twitter:title" content="<?= $judul ?>">
+	<meta name="twitter:description" content="<?= $deskripsi ?>">
+	<meta name="twitter:image" content="https://saitama.my.id<?= $cover ?>">
 
 	<script defer src='/main.js?<?= md5(rand()) ?>' type="module"></script>
 </head>
