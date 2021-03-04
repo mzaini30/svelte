6c6b067a30844bd4f003dd896f5e065291945b22
mzaini30 <muhzaini30@gmail.com>
2021-03-05 07:20:48 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
+import HMR from '@roxi/routify/hmr'
 import App from './App.svelte'
+const app = HMR(App, { target: document.body }, 'routify-app')
 
-const app = new App({
-  target: document.body
-})
+// const app = new App({
+//   target: document.body
+// })
 
 export default app
 
 // recreate the whole app if an HMR update touches this module
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => {
-    app.$destroy()
-  })
-  import.meta.hot.accept()
-}
+// if (import.meta.hot) {
+//   import.meta.hot.dispose(() => {
+//     app.$destroy()
+//   })
+//   import.meta.hot.accept()
+// }
