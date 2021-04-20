2c04057240f983a85c413dab739e7c13da245781
mzaini30 <muhzaini30@gmail.com>
2021-04-20 15:01:38 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({
+      manifest: {
+        display: 'minimal-ui'
+      }
+    }),
 	  svelte({
 	  	preprocess: preprocess()
 	  }),
