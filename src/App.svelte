a99567fa1b42332340e76fd3ca81f58ff484a33c
Zen <muhzaini30@gmail.com>
2022-12-26 06:12:19 +0800

    Oke

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script>
   import Router from "svelte-spa-router";
   import routes from "virtual:generated-pages-svelte";
-  import { App } from "framework7-svelte";
 </script>
 
-<App theme="ios">
-  <Router {routes} />
-</App>
+<Router {routes} />
 
 <style>
   :global(*) {
