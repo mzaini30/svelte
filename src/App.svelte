974377968d70d5e0d76b54a3cbd7aedfb18599be
Zen <muhzaini30@gmail.com>
2023-01-30 22:29:07 +0800

    typescript

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
   import Router from "svelte-spa-router";
+  // @ts-ignore
   import routes from "virtual:generated-pages-svelte";
   import "./tailwind.css";
 </script>
