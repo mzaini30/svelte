797f416def391856b40ec76ac5575c08b52ffddc
Zen <muhzaini30@gmail.com>
2023-01-30 21:08:28 +0800

    preprocess svelte postcss tailwind

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
 import pages from "vite-plugin-pages-svelte";
+import preprocess from "svelte-preprocess";
 
-let plugins = [svelte(), pages()];
+let plugins = [
+  svelte({
+    preprocess: [
+      preprocess({
+        postcss: true,
+      }),
+    ],
+  }),
+  pages(),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
