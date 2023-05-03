1552997c783d8ac0d9bc3da60a4d580759049e4b
Jasa Olah Website <muhzaini30@gmail.com>
2023-05-04 06:19:52 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,9 @@
   // @ts-ignore
   import routes from "virtual:generated-pages-svelte";
   import "@unocss/reset/tailwind.css";
-  import "warna-tailwind/warna.css";
+  // import "warna-tailwind/warna.css";
   import "./App.css";
+  import "uno.css";
 </script>
 
 <Router {routes} />
