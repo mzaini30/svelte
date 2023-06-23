75f64541f8379170698afa2bd6a5d53558a2a5a7
mzaini30 <muhzaini30@gmail.com>
2023-06-23 08:49:05 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
 import pages from "vite-plugin-pages-svelte";
 import uno_css from "unocss/vite";
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
-let plugins = [svelte(), pages(), uno_css()];
+
+let plugins = [svelte(), pages(), uno_css({
+  // ...
+  transformers: [
+    transformerVariantGroup(),
+  ],
+})];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
