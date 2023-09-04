1e5955451534a9ae916c62c1c5c6c29518864762
mzaini30 <muhzaini30@gmail.com>
2023-09-04 20:36:44 +0800

    kasih apply

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
 import pages from "vite-plugin-pages-svelte";
 import uno_css from "unocss/vite";
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import transformer_directives from "@unocss/transformer-directives";
 
-
-let plugins = [svelte(), pages(), uno_css({
-  // ...
-  transformers: [
-    transformerVariantGroup(),
-  ],
-})];
+let plugins = [
+  svelte(),
+  pages(),
+  uno_css({
+    // ...
+    transformers: [transformerVariantGroup(), transformer_directives()],
+  }),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
