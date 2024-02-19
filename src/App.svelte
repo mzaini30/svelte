cbb99fb15f6ff302107400d91b42cdf1d2d7286d
mzaini30 <muhzaini30@gmail.com>
2024-02-20 00:35:44 +0800

    nambah tailwind shadcdn

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,13 @@
   import { Router, createRouter } from '@roxi/routify'
   import routes from '../.routify/routes.default.js'
 
-  export const router = createRouter({ routes })
+  export const router = createRouter({
+    routes,
+    urlRewrite: {
+      toExternal: (url) => `#${url}`, // prepend URLs with #
+      toInternal: (url) => url.replace(/^.+#/, ''), // remove leading #
+    },
+  })
 </script>
 
 <Router {router} />
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import './app.pcss'
 import App from './App.svelte'
 
 new App({
-    target: document.body,
-    hydrate: import.meta.env.ROUTIFY_SSR_ENABLE,
+  target: document.body,
+  hydrate: import.meta.env.ROUTIFY_SSR_ENABLE,
 })
