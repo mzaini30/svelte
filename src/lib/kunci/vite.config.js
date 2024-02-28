9c031d38ebb80436ac4e63b62cadebdd75bb45c1
mzaini30 <muhzaini30@gmail.com>
2024-02-29 05:53:09 +0800

    hapus submodule kunci

--- src/lib/kunci/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import legacy from "@vitejs/plugin-legacy";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), legacy()],
-});
